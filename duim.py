1832c7d0c00fd1dae35a39caee8e404699eafa88
Jahanara123 <61766031+Jahanara123@users.noreply.github.com>
2021-12-02 19:45:50 -0500

    Update duim.py

--- duim.py
+++ duim.py
@@ -23,28 +23,81 @@ Description: <improved DU command, to get information in files>
 Date:2021-12-02
 '''
 
-def parse_command_args():
-    "Set up argparse here. Call this function inside main."
-    parser = argparse.ArgumentParser(description="DU Improved -- See Disk Usage Report with bar charts",epilog="Copyright 2021")
-    parser.add_argument("-l", "--length", type=int, default=20, help="Specify the length of the graph. Default is 20.")
-    # add argument for "human-readable". USE -H, don't use -h! -h is reserved for --help which is created automatically.
-    # check the docs for an argparse option to store this as a boolean.
-    # add argument for "target". set number of args to 1.
-    args = parser.parse_args()
+def du_sub(target_directory) -> list:
+    # create command list
+    command = ["du", "-d", "1", target_directory]
+    # call Popen to run the command
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # parse stdout, stderr from process
+    stdout, stderr = process.communicate()
+    # strip new line and split into list and return to caller
+    return [line.strip() for line in stdout.decode("UTF-8").strip().split('\n')]
 
 
-def percent_to_graph(percent, total_chars):
-    "returns a string: eg. '##  ' for 50 if total_chars == 4"
-    pass
+def percent_to_graph(percent, total_chars) -> str:
+    bar_graph = ""
+    if isinstance(percent, int) and (0 <= percent <= 100):
+        fill = int(total_chars * percent / 100)
+        bar_graph = "{:<{width}}".format("=" * fill, width=total_chars)
+    else:
+        raise ValueError
+    # return bar graph as string to caller
+    return bar_graph
 
-def call_du_sub(location):
-    "use subprocess to call `du -d 1 + location`, rtrn raw list"
-    pass
+def ceate_dir_dict(dirs: list) -> dict:
+    dir_dict = {}
+    for entry in dirs:
+        nbytes, directory = entry.split()
+        dir_dict[directory] = int(nbytes)
+    return dir_dict
 
-def create_dir_dict(raw_dat):
-    "get list from du_sub, return dict {'directory': 0} where 0 is size"
-    pass
+def get_total_size(dir_dict: list) -> int:
+    # return total number of bytes from directory dictionary
+    return sum(dir_dict.values())
 
+def get_unit(n):
+    # returns the corresponding unit
+    factor = 1000 # change to 1024 if required
+    if n < factor:
+        return 'B'
+    elif factor <= n < factor ** 2:
+        return 'KiB'
+    elif factor**2 <= n < factor**3:
+        return 'MiB'
+    elif factor**3 <= n < factor**4:
+        return 'GiB'
+    else:
+        return 'TiB'
+
+def auto_scale_n(n):
+    # scales output to be just 3 digits long
+    factor = 1000 # change to 1024 if required
+    scaled = n
+    while scaled > factor:
+        scaled /= factor
+    return scaled
 
 if __name__ == "__main__":
-    pass
+    _, *args = sys.argv
+    if len(args) == 0:
+        directory = "."
+    elif len(args) > 1 or not os.path.isdir(args[0]):
+        print("ERROR: Invalid number of arguments or path is not valid.")
+        exit(0)
+    else:
+        directory = args[0] 
+    
+    # call our functions to get the requried data
+    dir_dict = ceate_dir_dict(du_sub(directory))
+    total_size = get_total_size(dir_dict)
+
+    # max with taken by bar graph
+    graph_max_size = 20
+
+    # loop for each entry in directory dict and print required info with graph
+    for subdir, size in dir_dict.items():
+        percentage = int(size / total_size * 100)
+        graph = percent_to_graph(percentage, graph_max_size)
+        print("{:>2} % [{}] {:0.1f} {:<5}\t{}".format(percentage, graph, auto_scale_n(size),get_unit(size) , subdir))
+    # print the total of the parent directory
+    print("Total: {:0.1f} {}\t\t\t{}".format(auto_scale_n(total_size), get_unit(total_size), directory))
